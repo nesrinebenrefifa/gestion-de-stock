720dba714b17dc4a426579ff869b0e8601114c79
nesrinebenrefifa <benrefifanesrine@gmail.com>
2024-10-15 19:52:47 +0200

    f

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',  // Bind to all network interfaces
+    strictPort: true,  // Ensures Vite will fail if the port is already in use
+    port: 5173,        // Port number
+  },
   plugins: [react()],
 })
